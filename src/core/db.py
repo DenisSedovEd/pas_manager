e5d1807db32544985302c259d7bd519b03fdb6bd
SDE <deanisik@gmail.com>
2025-11-21 11:58:08 +0700

    add db make session file

---
+++ src/core/db.py
@@ -0,0 +1,31 @@
+from typing import AsyncGenerator, Any
+
+from sqlalchemy.ext.asyncio import (
+    AsyncSession,
+    AsyncEngine,
+    async_sessionmaker,
+    create_async_engine,
+)
+
+from src.core.config import settings
+
+async_engine: AsyncEngine = create_async_engine(
+    settings.db.url,
+    echo=settings.db.echo,
+    future=settings.db.future,
+)
+
+async_session: async_sessionmaker[AsyncSession] = async_sessionmaker(
+    bind=async_engine,
+    expire_on_commit=False,
+    class_=AsyncSession,
+)
+
+
+async def get_session() -> AsyncGenerator[AsyncSession, Any]:
+    async with async_session() as session:
+        try:
+            yield session
+        except Exception:
+            await session.rollback()
+            raise
